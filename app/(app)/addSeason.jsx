410e72e5efbb4c31c3704086563d53f8b283e0a8
jakefleming13 <113072839+jakefleming13@users.noreply.github.com>
2024-06-11 12:46:27 -0600

    Implement add player button that allows a user to add a player to the roster list, also add confirm button with no functionality

--- app/(app)/addSeason.jsx
+++ app/(app)/addSeason.jsx
@@ -2,13 +2,12 @@ import { View, Text, TextInput, ScrollView } from "react-native";
 import { useRouter } from "expo-router";
 import { useAuth } from "../../context/authContext";
 import { SafeView } from "../../components/SafeView";
-import { Pressable } from "react-native";
+import { Pressable, TouchableOpacity, StyleSheet } from "react-native";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import { COLORS } from "../../constants/Colors";
-import { StyleSheet } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 import { useState } from "react";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
@@ -16,88 +15,121 @@ import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 export default function addSeason() {
   const [teamName, setTeamName] = useState("");
 
-  const players = [
+  var [teamSize, setTeamSize] = useState(8);
+  var [players, setPlayers] = useState([
     {
       playerName: "",
       playerNumber: "",
+      playerID: 8,
     },
     {
       playerName: "",
       playerNumber: "",
+      playerID: 7,
     },
     {
       playerName: "",
       playerNumber: "",
+      playerID: 6,
     },
     {
       playerName: "",
       playerNumber: "",
+      playerID: 5,
     },
     {
       playerName: "",
       playerNumber: "",
+      playerID: 4,
     },
     {
       playerName: "",
       playerNumber: "",
+      playerID: 3,
     },
     {
       playerName: "",
       playerNumber: "",
+      playerID: 2,
     },
     {
       playerName: "",
       playerNumber: "",
+      playerID: 1,
     },
-  ];
+  ]);
+
+  const addPlayer = () => {
+    players.unshift({
+      playerName: "",
+      playerNumber: "",
+      playerID: teamSize + 1,
+    });
+    setTeamSize((teamSize += 1));
+  };
 
   const router = useRouter();
   return (
     <SafeView style={styles.container}>
       <View style={styles.cancelContainer}>
-        <Pressable onPress={() => router.push("seasons")}>
+        <TouchableOpacity onPress={() => router.push("seasons")}>
           <View style={styles.headerBtn}>
             <Text style={styles.headerBtnText}>CANCEL</Text>
           </View>
-        </Pressable>
+        </TouchableOpacity>
       </View>
       <View style={styles.titleContainer}>
         <Text style={styles.titleText}>Create new season</Text>
       </View>
-      <View style={styles.titleContainer}>
-        <View style={styles.inputContainer}>
-          <TextInput
-            autoCapitalize="none"
-            autoCorrect={false}
-            keyboardType="default"
-            maxLength={18}
-            onChangeText={(value) => setTeamName(value)}
-            placeholder="Team Name...              "
-            style={styles.input}
-            onFocus={this.onFocus}
-          />
-        </View>
-        <View style={styles.inputContainer}>
-          <TextInput
-            autoCapitalize="none"
-            autoCorrect={false}
-            keyboardType="default"
-            inputMode="numeric"
-            maxLength={12}
-            onChangeText={(value) => setTeamName(value)}
-            placeholder="Year...                 "
-            style={styles.input}
-          />
-        </View>
-        <View style={styles.seperator} />
-      </View>
-      <View style={styles.titleContainer}>
-        <Text style={styles.rosterTitleText}>Roster</Text>
-      </View>
       <ScrollView>
+        <View style={styles.titleContainer}>
+          <View style={styles.inputContainer}>
+            <TextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              keyboardType="default"
+              maxLength={18}
+              onChangeText={(value) => setTeamName(value)}
+              placeholder="Team Name...              "
+              style={styles.input}
+              onFocus={this.onFocus}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <TextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              keyboardType="default"
+              inputMode="numeric"
+              maxLength={12}
+              onChangeText={(value) => setTeamName(value)}
+              placeholder="Year...                 "
+              style={styles.input}
+            />
+          </View>
+          <View style={styles.seperator} />
+        </View>
+        <View style={styles.titleContainer}>
+          <Text style={styles.rosterTitleText}>Roster</Text>
+        </View>
+        <View style={styles.rosterTitleContainer}>
+          <TouchableOpacity onPress={addPlayer}>
+            <View style={styles.addBtn}>
+              <Text style={styles.addBtnText}>ADD PLAYER</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+
         {players.map((player) => {
-          return <RosterPlayer />;
+          return <RosterPlayer playerID={player.playerID} />;
         })}
+        <View style={styles.confirmContainer}>
+          <TouchableOpacity>
+            <View style={styles.confirmBtn}>
+              <Text style={styles.confirmBtnText}>CONFRIM</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     </SafeView>
   );
@@ -111,7 +143,7 @@ const styles = StyleSheet.create({
   cancelContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
-    height: hp(11),
+    height: hp(10),
   },
   headerBtn: {
     width: "80%",
@@ -133,7 +165,7 @@ const styles = StyleSheet.create({
   titleText: {
     fontSize: RFValue(30),
     color: COLORS.primary,
-    marginBottom: 35,
+    marginBottom: 20,
   },
   titleContainer: {
     alignItems: "center",
@@ -146,7 +178,6 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     justifyContent: "center",
     padding: 15,
-    alignItems: "flex-start",
   },
   input: {
     flex: 1,
@@ -165,10 +196,34 @@ const styles = StyleSheet.create({
     fontSize: RFValue(18),
     color: COLORS.primary,
     fontWeight: "bold",
-    marginBottom: 30,
+  },
+  rosterTitleContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    height: hp(11),
+  },
+  addBtn: {
+    width: "80%",
+    height: hp(7),
+    backgroundColor: COLORS.primary,
+    borderRadius: 20,
+    marginHorizontal: 20,
+    marginTop: 18,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  addBtnText: {
+    fontSize: RFValue(9),
+    padding: 7,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: COLORS.white,
+  },
+  playerIDText: {
+    marginLeft: 10,
   },
   rosterIcon: {
-    marginLeft: 20,
+    marginLeft: 5,
   },
   rosterPlayerContainer: {
     flexDirection: "row",
@@ -213,11 +268,35 @@ const styles = StyleSheet.create({
     marginTop: RFValue(4),
     fontSize: RFValue(10),
   },
+  confirmBtn: {
+    width: RFValue(50),
+    height: hp(7),
+    backgroundColor: COLORS.primary,
+    borderRadius: 20,
+    marginTop: 18,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  confirmBtnText: {
+    fontSize: RFValue(9),
+    padding: 7,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: COLORS.white,
+  },
+  confirmContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    height: hp(13),
+  },
 });
 
-const RosterPlayer = () => {
+const RosterPlayer = ({ playerID }) => {
   return (
     <View style={styles.rosterPlayerContainer}>
+      <Text style={styles.playerIDText}>
+        {playerID < 10 ? playerID + "  " : playerID}
+      </Text>
       <FontAwesome5
         name="user-alt"
         style={styles.rosterIcon}
