03788513ca9070dcf05596e228fd7e7ef3fece9a
jakefleming13 <113072839+jakefleming13@users.noreply.github.com>
2024-06-27 18:57:34 -0600

    add playerStats screen, basic layout with no functionality

---
+++ app/(app)/playerStats.jsx
@@ -0,0 +1,122 @@
+import { View, Text, ScrollView } from "react-native";
+import { useRouter, useLocalSearchParams } from "expo-router";
+import { SafeView } from "../../components/SafeView";
+import { TouchableOpacity } from "react-native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { COLORS } from "../../constants/Colors";
+import { StyleSheet } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { AntDesign } from "@expo/vector-icons";
+
+export default function playerStats() {
+  const router = useRouter();
+  const params = useLocalSearchParams();
+  const { teamName, year, seasonID } = params;
+
+  return (
+    <SafeView style={styles.container}>
+      <View style={styles.backContainer}>
+        <TouchableOpacity
+          onPress={() =>
+            router.push({
+              pathname: "seasonHome",
+              params: { teamName: teamName, year: year, seasonID: seasonID },
+            })
+          }
+        >
+          <View style={styles.headerBtn}>
+            <AntDesign
+              style={styles.backIcon}
+              name="left"
+              size={hp(3.7)}
+              color={COLORS.white}
+            />
+            <Text style={styles.headerBtnText}>HOME</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.titleContainer}>
+        <Text style={styles.titleText}>
+          {teamName}, {year}
+        </Text>
+      </View>
+      <ScrollView>
+        <View style={styles.seperator} />
+
+        <View style={styles.titleContainer}>
+          <Text style={styles.secondaryTitleText}>Player Stats</Text>
+        </View>
+        <View style={styles.titleContainer}>
+          <Text style={styles.spacerText}></Text>
+        </View>
+      </ScrollView>
+    </SafeView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  backContainer: {
+    flexDirection: "row",
+    justifyContent: "start",
+    height: hp(11),
+  },
+  headerBtn: {
+    flexDirection: "row",
+    width: "40%",
+    height: hp(7),
+    backgroundColor: COLORS.primary,
+    borderRadius: 20,
+    marginHorizontal: 20,
+    marginTop: 18,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headerBtnText: {
+    fontSize: RFValue(9),
+    paddingRight: 3,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: COLORS.white,
+  },
+  titleText: {
+    fontSize: RFValue(30),
+    color: COLORS.primary,
+    marginBottom: 35,
+  },
+  titleContainer: {
+    alignItems: "center",
+  },
+  secondaryTitleText: {
+    fontSize: RFValue(18),
+    color: COLORS.primary,
+    marginBottom: 15,
+  },
+  tertiaryTitleText: {
+    fontSize: RFValue(18),
+    color: COLORS.primary,
+    marginBottom: 15,
+    marginTop: 35,
+  },
+  spacerText: {
+    fontSize: RFValue(18),
+    color: COLORS.primary,
+    marginTop: 5,
+  },
+  seperator: {
+    borderBottomColor: COLORS.primary,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    width: "60%",
+    alignSelf: "center",
+    marginBottom: 35,
+  },
+  backIcon: {
+    paddingRight: 5,
+  },
+});
